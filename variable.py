bbc4ce1dd6a360324fc5a0229e7793182b3e1bf6
sumeeth18 <sumeeth.yarlagadda4@gmail.com>
2022-10-13 15:38:29 +0530

    variable file added

---
+++ variable.py
@@ -0,0 +1,17 @@
+#different types of variables 
+# syntax <variable name> = variable
+# variable name should always starts with alphabets,if want to give spaces then it should be in "_" eg a= 20, a_b=20
+#initial variable name are not allowed with Special characters and numbers  
+a = 20 # valid and variable value is integer(In python data types are automatically analysed)
+b = 3.15 # valid and variable value in floating
+c = 'value' #string value should always be in '' or ""
+d = False #boolean value
+e = None # Null Value
+
+#type is built in function to know the type of value
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(d)
+print(e)
